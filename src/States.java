5fd4af11689482d001459164f672ea2b278a6175
Jolanrensen <j.j.r.rensen@student.tue.nl>
2016-03-02 17:09:56 +0100

    added states
    added shit

--- src/States.java
+++ src/States.java
@@ -11,13 +11,73 @@ enum States implements State {
 	Rest {
 		@Override
 		public State next(Input input) {
-			LCD.drawString("Resting now", 0, 0);
-			
+			//ask user to press enter if tube is empty
+			//LCD.drawString("Resting now", 0, 0);
+			if (input.buttonEnterDown()) {
+				return Waiting;
+			} else {
 			return Rest;
+			}
 		}
 	},
 	
-	WaitForButton {
+	Waiting {
+		@Override
+		public State next(Input input) {
+			//display nr of inserted disks and enter to start the sorting
+			if (input.touchDown()) {
+				//count++
+				return DiskAdd;
+			}
+			if (input.buttonEnterDown()) {
+				return AcceptDisk2;
+			}
+			return Waiting;
+		}
+	},
+	
+	DiskAdd {
+		@Override
+		public State next(Input input) {
+			if (!input.touchDown()) {
+				return Waiting;
+			} else {
+				return DiskAdd;
+			}
+			
+		}
+	},
+	
+	AcceptDisk2 {
+		@Override
+		public State next(Input input) {
+			//display the state, count
+			//thus needs counter as input
+			if (countIsGreaterThanZero()) {
+				return ExpectsDisk;
+			} else {
+				return ExpectsFinished;
+			}
+		}
+	},
+	
+	ExpectsDisk {
+		@Override
+		public State next(Input input) {
+			//input from the color sensor
+			//display nr of disks, plus nr of black en white
+			if (colorSensorWhite() && !colorSensorBlack() && !colorSensorGrey()) {
+				//do count--
+				//do motor angle++
+				//do black counter++
+				//wait until done
+				return AcceptDisk2;
+			} else if (colorSensorBlack() &&)
+			
+		}
+	},
+	
+	WaitForButton { //sample
 		@Override
 		public State next(Input input) {
 			if (input.buttonSSDown()) {
