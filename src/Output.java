fde080649d8491db83f5265c1f977ae62a499d72
s158881 <s158881@s158881.campus.tue.nl>
2016-03-04 12:41:26 +0100

    vital changes

--- src/Output.java
+++ src/Output.java
@@ -13,7 +13,7 @@ import lejos.utility.Delay;
 public class Output {
 	// Store the state variables object.
 	private StateVariables sv;
-	private int turndegrees 288;
+	int turndegrees 288;
 	
 	public Output(StateVariables sv) {
 		this.sv = sv;
@@ -70,6 +70,6 @@ public class Output {
 	}
 	
 	public void MotorSortWhite() { // When the colorsensor detects a white disk, turn one left.
-		Motor.A.rotate(turndegrees, false);
+		Motor.A.rotate(-turndegrees, false);
 	}
 }
