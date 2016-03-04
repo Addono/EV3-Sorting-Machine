6c96717b9cc4def5b2dc9eefc8a4a460b64cce95
Adriaan Knapen <a.d.knapen@student.tue.nl>
2016-03-04 10:51:52 +0100

    Added program to test the values of the input class.

---
+++ src/InputTest.java
@@ -0,0 +1,50 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class InputTest {
+
+	public static void main(String[] args) {
+		Input input = new Input();
+		int menu = 1;
+		
+		boolean leftButtonOld = false;
+		boolean rightButtonOld = true;
+		
+		while(!Button.ESCAPE.isDown()) {
+			if(Button.LEFT.isDown() && !leftButtonOld) {
+				menu--;
+			}
+
+			leftButtonOld = Button.LEFT.isDown();
+
+			if(Button.RIGHT.isDown() && !rightButtonOld) {
+				menu++;
+			}
+
+			rightButtonOld = Button.RIGHT.isDown();
+
+			LCD.clear();
+			switch(menu % 2) {
+			case 0:
+				LCD.drawString("Touch input", 3, 0);
+				LCD.drawString("SS    : " + input.buttonSSDown(), 0, 1);
+				LCD.drawString("Yes   : " + input.buttonYesDown(), 0, 2);
+				LCD.drawString("No    : " + input.buttonNoDown(), 0, 3);
+				LCD.drawString("Touch : " + input.touchDown(), 0, 4);
+				break;
+
+			case 1:
+				input.updateColor();
+				LCD.drawString("Color input", 3, 0);
+				LCD.drawString("White : " + input.colorSensorWhite(), 0, 1);
+				LCD.drawString("Black : " + input.colorSensorBlack(), 0, 2);
+				LCD.drawString("Red   : " + input.colorSensorRed(), 0, 3);
+				LCD.drawString("Grey  : " + input.colorSensorGrey(), 0, 4);
+				break;
+			}
+			Delay.msDelay(100);
+		}
+	}
+
+}
