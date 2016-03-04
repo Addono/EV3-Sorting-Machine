93ef9931571b3a075361baf78c9b69b1f207a511
s158881 <s158881@s158881.campus.tue.nl>
2016-03-04 11:54:30 +0100

    State Variables class and functions

--- src/StateVariables.java
+++ src/StateVariables.java
@@ -1,18 +1,18 @@
 public class StateVariables {
 
-	private int diskcounter = 0;
-	private int whitecounter = 0;
-	private int blackcounter = 0;
+	private int diskcounter = 0; //counter that will get increased when you put in a disk
+	private int whitecounter = 0; // counter that will get increased when a white disk is sorted
+	private int blackcounter = 0; // counter that will get increased when a black disk is sorted
 
-	private void increaseCounter() {
+	private void increaseCounter() { // increase diskcounter
 		diskcounter++;
 	}
 
-	private void increaseWhiteCounter() {
+	private void increaseWhiteCounter() { // increase whitecounter
 		whitecounter++;
 	}
 
-	private void increaseBlackCounter() {
+	private void increaseBlackCounter() { // increase blackcounter
 		blackcounter++;
 	}
 }	
