972809f446dd5029720e6f85dc5729c64fc30011
s158881 <s158881@s158881.campus.tue.nl>
2016-03-04 11:52:09 +0100

    State variables class and functions

---
+++ src/StateVariables.java
@@ -0,0 +1,19 @@
+public class StateVariables {
+
+	private int diskcounter = 0;
+	private int whitecounter = 0;
+	private int blackcounter = 0;
+
+	private void increaseCounter() {
+		diskcounter++;
+	}
+
+	private void increaseWhiteCounter() {
+		whitecounter++;
+	}
+
+	private void increaseBlackCounter() {
+		blackcounter++;
+	}
+}	
+	
