eeb5a8a190e78f62bad087afe52a82c960949c57
Adriaan Knapen <a.d.knapen@student.tue.nl>
2016-03-04 12:01:49 +0100

    Added program to test output.

---
+++ src/OutputTest.java
@@ -0,0 +1,65 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class OutputTest {
+
+	public static void main(String[] args) {
+		Output o = new Output();
+		
+		int menu = 1; // Stores which page is currently on top.
+		
+		boolean leftButtonOld = false;
+		boolean rightButtonOld = false;
+		
+		boolean enterButtonOld = false;
+		boolean enterButtonPressed = false;
+		
+		while(!Button.ESCAPE.isDown()) {
+			// Manage button input.
+			if(Button.LEFT.isDown() && !leftButtonOld) {
+				menu--;
+			}
+
+			leftButtonOld = Button.LEFT.isDown();
+
+			if(Button.RIGHT.isDown() && !rightButtonOld) {
+				menu++;
+			}
+
+			rightButtonOld = Button.RIGHT.isDown();
+			
+			if(Button.ENTER.isDown() && !enterButtonOld) {
+				enterButtonPressed = true;
+			} else {
+				enterButtonPressed = false;
+			}
+			
+			// Start drawing on the screen.
+			LCD.clear();
+			LCD.drawString("Press Enter to run", 1, 5);
+			
+			// Page specific content.
+			switch(menu % 2) {
+			case 0:
+				LCD.drawString("Motor Sort Black", 3, 0);
+			
+				if(enterButtonPressed) {
+					o.MotorSortBlack();
+				}
+				break;
+
+			case 1:
+				LCD.drawString("Motor Sort White", 3, 0);
+				
+				if(enterButtonPressed) {
+					o.MotorSortWhite();
+				}
+				break;
+			}
+			
+			// Cap refresh rate (screen can't keep up with processor speed).
+			Delay.msDelay(50);
+		}
+	}
+}
