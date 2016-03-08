bf80cd041241833ba6ffa6c823b12b98afbbea1f
s154563 <s154563@s154563.campus.tue.nl>
2016-03-08 10:25:26 +0100

    gray

--- src/States.java
+++ src/States.java
@@ -30,7 +30,7 @@ enum States implements State {
 		public State next(Input i, Output o) {
 			i.updateColor(); // Sample the sensor for new color data. 
 			
-			if (i.colorSensorGrey()) {
+			if (i.colorSensorGrey()&&!i.colorSensorBlack()) {
 				return Waiting;
 			} else if (i.colorSensorBlack() || i.colorSensorWhite()) {
 				return SortDisksNoCounting;
